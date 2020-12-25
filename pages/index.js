6f0df0d2c0aa35701be66f8c1d248f16b1939b11
walebant <wale.ashiru04@gmail.com>
2020-12-25 10:36:33 +0100

    ui

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,70 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const generateInvoice = async () => {
+    setIsLoading(true);
+
+    const fetchData = async () => {
+      const data = await fetch('http://localhost:3000/api/generate-invoice', {
+        method: 'POST',
+        body: name,
+      });
+      return data.arrayBuffer();
+    };
+
+    const saveAsPDF = async () => {
+      const buffer = fetchData();
+      const blob = new Blob([buffer]);
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'somedoc.pdf';
+      link.click();
+      setIsLoading(false);
+      alert('Invoice downloaded');
+    };
+
+    saveAsPDF();
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Hello {name || 'Customer'} 👋</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Fill the form below to generate your invoice
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <form className={styles.form}>
+          <div className={styles.field}>
+            <label for='name'>Enter Name</label>
+            <input
+              id='name'
+              type='text'
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <button
+            onClick={() => generateInvoice()}
+            disabled={isLoading}
+            className={styles.button}>
+            Download Invoice
+          </button>
+        </form>
+        {isLoading && <p>Generating Invoice... 🤞🏽</p>}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
